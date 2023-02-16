6e376cc66f7268f5838f5e233168e176577b429b
MIBismuth <jodulopes@gmail.com>
2023-02-16 18:19:34 +0000

    adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
- 
-export default {
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   kit: {
-    // default options are shown
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
- 
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    })
-  }
+    adapter: adapter(),
+  },
+  preprocess: vitePreprocess()
 };
+
+export default config;
